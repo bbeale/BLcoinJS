214dc62f17a7dba1ed83821abef5661653083aac
Ben <beale.ben@gmail.com>
2021-01-27 20:38:22 -0500

    Added 3 new data methods.

--- dev/BLcoin.js
+++ dev/BLcoin.js
@@ -80,4 +80,54 @@ BLcoin.prototype.chainIsValid = function (blockChain) {
   return validChain;
 };
 
+BLcoin.prototype.getBlock = function (blockHash) {
+  let correctBlock = null;
+  this.chain.forEach(block => {
+    if (block.hash === blockHash)
+      correctBlock = block;
+  });
+  return correctBlock;
+};
+
+BLcoin.prototype.getTransaction = function (transactionId) {
+  let correctTransaction = null;
+  let correctBlock = null;
+  this.chain.forEach(block => {
+    block.transactions.forEach(transaction => {
+      if (transaction.transactionId === transactionId) {
+        correctTransaction = transaction;
+        correctBlock = block;
+      };
+    });
+  });
+
+  return {
+    transaction: correctTransaction,
+    block: correctBlock
+  };
+};
+
+BLcoin.prototype.getAddressData = function (address) {
+  const addressTransactions = [];
+  let balance = 0;
+
+  this.chain.forEach(block => {
+    block.transactions.forEach(transaction => {
+      if (transaction.sender === address || transaction.recipient === address) {
+        addressTransactions.push(transaction);
+      }
+    });
+  });
+
+  addressTransactions.forEach(transaction => {
+    if (transaction.recipient === address) balance += transaction.amount;
+    else if (transaction.sender === address) balance -= transaction.amount;
+  });
+
+  return {
+    addressTransactions: addressTransactions,
+    addressBalance: balance
+  };
+};
+
 module.exports = BLcoin;
